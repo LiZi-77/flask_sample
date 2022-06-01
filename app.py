917c697663dea0815ff58f0f1f18115251877d69
Ziqian Li <liz_q@outlook.com>
2022-06-01 23:19:34 +0200

    add 404.html

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def index():
     movies = Movie.query.all()
     return render_template('index.html', user=user, movies=movies)
 
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html', user=user), 404  # 返回模板和状态码
+
 #Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:22055@localhost/flask_sample'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
